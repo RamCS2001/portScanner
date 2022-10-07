f8ccaae90a3e00aa988356622d8467532a795f41
Ramkumar Karthikeyan <kram.cse.2001@gmail.com>
2022-10-07 11:43:55 +0530

    ip input

--- portScanning.py
+++ portScanning.py
@@ -1,8 +1,9 @@
 import socket
-ports=[80,443,3306]
+ports=[80,443,3306,5000]
+ip= input("Enter the ip addr to scan: ")
 for i in ports:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    response = sock.connect_ex(('127.0.0.1', i))
+    response = sock.connect_ex((ip, i))
     if response == 0:
         print("Port ", i ," is open")
     else:
